f0a12edb404399c0458265370f743534c41b55c2
kazaky <kazak@microfocus.com>
2019-06-30 13:38:54 +0300

    Refactor the wordsegmentation to allow concurrent runs

--- wordsegmentation_test.go
+++ wordsegmentation_test.go
@@ -8,8 +8,9 @@ import (
 )
 
 func TestSegment(t *testing.T) {
+
 	expected := []string{"what", "is", "the", "weather", "like", "today"}
 	englishCorpus := c.NewEnglishCorpus()
-
-	assert.Equal(t, Segment(englishCorpus, "WhatIsTheWeatherliketoday? "), expected)
+	segmentaor := NewSegmenator(englishCorpus)
+	assert.Equal(t, segmentaor.Segment(englishCorpus, "WhatIsTheWeatherliketoday? "), expected)
 }
