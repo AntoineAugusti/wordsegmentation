e5cae09bdc366c2774c9e1f8f76d808e8bafb058
kazaky <kazak@microfocus.com>
2019-06-30 13:51:55 +0300

    fix compliation errors

--- wordsegmentation_test.go
+++ wordsegmentation_test.go
@@ -7,10 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSegment(t *testing.T) {
-
-	expected := []string{"what", "is", "the", "weather", "like", "today"}
+func TestSegmenator_Segment(t *testing.T) {
+	type args struct {
+		Text string
+	}
 	englishCorpus := c.NewEnglishCorpus()
-	segmentaor := NewSegmenator(englishCorpus)
-	assert.Equal(t, segmentaor.Segment("WhatIsTheWeatherliketoday?"), expected)
+
+	tests := []struct {
+		name string
+		s    *Segmenator
+		args args
+		want []string
+	}{
+		{"basic", NewSegmenator(englishCorpus), args{"WhatIsTheWeatherliketoday?"}, []string{"what", "is", "the", "weather", "like", "today"}},
+		{"basic with spaces", NewSegmenator(englishCorpus), args{"click me next"}, []string{"click", "me", "next"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.s.Segment(tt.args.Text)
+			assert.Equalf(t, tt.want, got, fmt.Sprintf("Segmenator.Segment() = %v, want %v\n", got, tt.want))
+		})
+	}
 }
